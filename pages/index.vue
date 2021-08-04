d9f393c3fa04d7af04e38d160462c1df6897c4c4
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2021-08-04 10:38:54 +0200

    Featured Product

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="container">
-    <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
-  </div>
+  <main class="listingPage">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 md-4 col-sm-4">
+          <!-- Sidebar -->
+        </div>
+
+        <!-- Main Content -->
+        <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
+          <featured-product />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {}
+import FeaturedProduct from '../components/FeaturedProduct.vue'
+export default {
+  components: { FeaturedProduct },}
 </script>
