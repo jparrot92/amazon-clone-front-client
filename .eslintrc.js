9d9462a51567aa931284fb2d6659c637a9aece19
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2021-08-04 14:56:10 +0200

    Config Eslint/Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
   extends: [
     '@nuxtjs',
+    'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
-    'prettier'
   ],
-  plugins: [
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
