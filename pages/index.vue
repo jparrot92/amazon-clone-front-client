a9b472449b3346b356bf3440b3aca17d77738022
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2021-08-05 00:10:14 +0200

    HTML Listing page

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,124 @@
         <!-- Main Content -->
         <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
           <featured-product />
+
+          <div class="mainResults">
+            <ul class="s-result-list">
+              <li class="s-result-item celwidget">
+                <div class="s-item-container">
+                  <!-- Best seller -->
+                  <div class="a-spacing-micro">
+                    <div class="bestSeller">
+                      <a href="#">Best Seller</a>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div class="row">
+                      <!-- Image -->
+                      <div class="col-sm-3 text-center">
+                        <a href="#">
+                          <img
+                            src="product.photo"
+                            style="width: 150px"
+                            class="img-fluid"
+                          />
+                        </a>
+                      </div>
+
+                      <div class="col-sm-9">
+                        <div class="a-row a-spacing-small">
+                          <!-- Title and Date -->
+                          <a href="#" class="a-size-medium">
+                            Harry Potter
+                            <span class="a-letter-spcae"></span>
+                            <span class="a-letter-spcae"></span>
+                            <span class="a-size-small a-color-secondary">
+                              Sep 3, 2019
+                            </span>
+                          </a>
+                        </div>
+
+                        <!-- Author's name -->
+                        <div class="a-row a-spacing-small">
+                          <span class="a-size-small a-color-secondary">by</span>
+                          <span class="a-size-small a-color-secondary"></span>
+                          <span class="a-size-small a-color-secondary">
+                            JK Rowling
+                          </span>
+                        </div>
+
+                        <!-- Shipment -->
+                        <div class="a-row">
+                          <span class="a-size-small">Ships to USA</span>
+                        </div>
+
+                        <div class="row">
+                          <div class="col-sm-7">
+                            <div class="a-row a-spacing-none">
+                              <a href="#" class="a-link-normal a-text-normal">
+                                Hardcover
+                              </a>
+                            </div>
+
+                            <!-- Price -->
+                            <div class="a-row a-spacing-none">
+                              <a href="#" class="a-link-normal a-text-normal">
+                                <span class="a-offscreen">$99</span>
+                                <span class="a-color-base sx-zero-spacing">
+                                  <span class="sx-price sx-price-large">
+                                    <sup class="sx-price-currency">$</sup>
+                                    <span class="sx-price-whole">99</span>
+                                    <sup class="sx-price-fractional">00</sup>
+                                  </span>
+                                </span>
+                              </a>
+                              <span class="a-letter-space"></span>
+                              <span
+                                class="
+                                  a-size-base-plus
+                                  a-color-secondary
+                                  a-text-strike
+                                "
+                              >
+                                $28.00
+                              </span>
+                            </div>
+
+                            <!-- Audible Trial -->
+                            <div class="a-row a-spacing-none">
+                              <span class="a-size-small a-color-secondary">
+                                Free with Audible trial
+                              </span>
+                            </div>
+                            <hr />
+
+                            <!-- Other Formats -->
+                            <span class="a-size-small a-color-secondary">
+                              Other Formats:
+                              <span class="a-letter-space"></span>
+                              <a
+                                href="#"
+                                class="a-size-small a-link-normal a-text-normal"
+                              >
+                                Audio CD
+                              </a>
+                            </span>
+                          </div>
+                          <!-- Ratings -->
+                          <div class="col-sm-5">
+                            <div class="a-row a-spacing-mini">
+                              <!-- Star Ratings -->
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -16,7 +134,8 @@
 </template>
 
 <script>
-import FeaturedProduct from '../components/FeaturedProduct.vue'
+import FeaturedProduct from '../components/FeaturedProduct.vue';
 export default {
-  components: { FeaturedProduct },}
+  components: { FeaturedProduct },
+};
 </script>
