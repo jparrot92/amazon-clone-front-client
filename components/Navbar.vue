7eece20559d28edcdb70b96fc90540f1604b6f03
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2021-08-16 23:01:12 +0200

    Data navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -87,21 +87,38 @@
                 </span>
               </a>
               <span class="icp-nav-link-border"></span>
-              <nuxt-link
-                id="nav-link-accountList"
-                to="/register"
-                class="nav-a nav-a-2"
-                tabindex="0"
-              >
-                <span class="nav-line-1">Hello, Sign in</span>
-                <span class="nav-line-2">
-                  Account &amp; Lists
-                  <span
-                    class="nav-icon nav-arrow"
-                    style="visibility: visible"
-                  ></span>
-                </span>
-              </nuxt-link>
+
+              <template v-if="$auth.loggedIn">
+                <nuxt-link
+                  id="nav-link-accountList"
+                  to="/register"
+                  class="nav-a nav-a-2"
+                  tabindex="0"
+                >
+                  <span class="nav-line-1">Hello,</span>
+                  <span class="nav-line-2">
+                    {{ $auth.$state.user.name }}
+                  </span>
+                </nuxt-link>
+              </template>
+
+              <template v-else>
+                <nuxt-link
+                  id="nav-link-accountList"
+                  to="/register"
+                  class="nav-a nav-a-2"
+                  tabindex="0"
+                >
+                  <span class="nav-line-1">Hello, Sign in</span>
+                  <span class="nav-line-2">
+                    Account &amp; Lists
+                    <span
+                      class="nav-icon nav-arrow"
+                      style="visibility: visible"
+                    ></span>
+                  </span>
+                </nuxt-link>
+              </template>
               <nuxt-link to="/orders" class="nav-a nav-a-2 nav-single-row-link">
                 <span class="nav-line-1"></span>
                 <span class="nav-line-2">Orders</span>
